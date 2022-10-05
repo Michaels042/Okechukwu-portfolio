c6bb3a542b2b83414eda14a6711cd6a3db08d690
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-10-05 08:37:43 +0100

    added hero-logo

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// setTimeout(function () {
-//   window.location.href = "https://michaels042.github.io/Okechukwu-portfolio/";
-// }, 5000);
-
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav");
 const desktop_menu = document.querySelector(".top-nav");
@@ -26,45 +22,3 @@ function readMoreJs() {
   let element = document.getElementById("moreJs");
   element.classList.toggle("show");
 }
-
-// function readMoreHtml() {
-//     var mhtml = document.getElementById("moreHtml");
-//     if (mhtml.style.color === "black") {
-//       mhtml.style.color = "white";
-//     } else {
-//       mhtml.style.color = "black";
-//     };
-//   };
-
-//   function readMoreCss() {
-//     var mcss = document.getElementById("moreCss");
-//     if (mcss.style.color === "black") {
-//       mcss.style.color = "white";
-//     } else {
-//       mcss.style.color = "black";
-//     };
-//   };
-
-//   function readMoreJs() {
-//     var mjs = document.getElementById("moreJs");
-//     if (mjs.style.color === "black") {
-//       mjs.style.color = "white";
-//     } else {
-//       mjs.style.color = "black";
-//     };
-//   };
-
-// const parentContainer = document.querySelector('.details');
-// parentContainer.addEventListener('click', event=> {
-//     const current = event.target;
-//     const isReadMoreBtn = current.className.includes('read-more-btn');
-//     if(!isReadMoreBtn) return;
-//     const currentText = event.target.parentNode.querySelector('.read-more-text');
-//     currentText.classList.toggle('read-more-text--show');
-//     current.textContent = current.textContent.includes('Read More') ?
-//     "Read Less" : "Read More";
-// });
-
-// document.addEventListener('click', function handleClick(event) {
-//     event.target.classList.add('bg-yellow');
-//   });
